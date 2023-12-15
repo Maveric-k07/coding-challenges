e3f652eefc369f2a2a9d616286336ec9f5cba51e
Akhil <gurramakhileshwar333@gmail.com>
2023-12-15 16:08:59 +0530

    add -m flag and support no flags

--- wc/main.go
+++ wc/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"unicode"
+	"unicode/utf8"
 )
 
 func main() {
@@ -13,6 +14,7 @@ func main() {
 	cFlag := flag.Bool("c", false, "count the number of bytes in a file")
 	lFlag := flag.Bool("l", false, "count the number of lines in a file")
 	wFlag := flag.Bool("w", false, "count the number of words in a file")
+	mFlag := flag.Bool("m", false, "count the number of characters in a file")
 	flag.Parse()
 
 	args := flag.Args()
@@ -22,6 +24,26 @@ func main() {
 	} else {
 		fmt.Println("File path is required")
 		os.Exit(1)
+	}
+
+	if !(*cFlag || *lFlag || *wFlag || *mFlag) {
+		byteCount, err := countBytes(filePath)
+		if err != nil {
+			fmt.Println("Error counting bytes:", err)
+			os.Exit(1)
+		}
+		lines, err := countLines(filePath)
+		if err != nil {
+			fmt.Println("Error counting lines:", err)
+			os.Exit(1)
+		}
+		words, err := countWords(filePath)
+		if err != nil {
+			fmt.Println("Error counting words:", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%10d %10d %10d %s\n", lines, words, byteCount, filePath)
 	}
 
 	if *cFlag {
@@ -49,6 +71,15 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Printf("%10d %s\n", words, filePath)
+	}
+
+	if *mFlag {
+		charCount, err := countCharacters(filePath)
+		if err != nil {
+			fmt.Println("Error counting characters:", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%10d %s\n", charCount, filePath)
 	}
 }
 
@@ -120,4 +151,30 @@ func splitWords(text string) []string {
 	}
 
 	return words
+}
+
+func countCharacters(filePath string) (int, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	charCount := 0
+
+	buffer := make([]byte, 4096)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil && err != bufio.ErrBufferFull {
+			break
+		}
+
+		charCount += utf8.RuneCount(buffer[:n])
+		if err == bufio.ErrBufferFull {
+			continue
+		}
+		break
+	}
+
+	return charCount, nil
 }
